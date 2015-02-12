ddd7b9b17e4e24787f9688cf300e2e8e724a610a
Jason Jalbuena <forlogos@users.noreply.github.com>
2015-02-12 08:16:54 -0500

    done! Everything works as expected, no errors.

--- bc2en.php
+++ bc2en.php
@@ -1,18 +1,42 @@
-<?php
+<?php //////////////////////////////////
+//Basecamp settings
 //your contact email or URL
 $contact='';
 
-//your basecamp user info
+//basecamp user info
 $username  = '';
 $password = '';
-$account_id = '';
-$basecamphq_url='';
+$account_id = '';//get this from the URL when you view 'My info'
+$basecamphq_url='';//usually https://companyname.basecamphq.com
 
-//set UR: endpoints
+//////////////////////////////////
+//Evernote settings
+
+//your developer token. More info: https://dev.evernote.com/doc/articles/authentication.php#devtoken
+$developerToken = "";
+
+//path to evernote autoload.php, from the SDK https://github.com/evernote/evernote-cloud-sdk-php
+$evernote_autoload='';// like 'evernote/'
+
+//the unique ID for the note you want updated. get this from URL when viewing you note in its own window on the evernote web app
+//https://sandbox.evernote.com/view/notebook/this-is-the-guid
+//everything after a ? or # is not part of the guid
+$note_guid ='';
+
+//evernote sandbox or no? set this to false to use in production
+$sandbox = true;
+
+//settings done, on to using them with the APIs
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//Get todos from BaseCamp
+
+//set URL: endpoints for basecamp
 $projects_URL='/projects.xml';
 $todo_URL='/todo_lists.xml';
 
-// Get cURL resource
+//Get cURL resource
 $curl = curl_init();
 
 //projects: Set some curl options
@@ -21,8 +45,7 @@ curl_setopt_array($curl, array(
     CURLOPT_USERAGENT => $contact,
     CURLOPT_URL => $basecamphq_url.$projects_URL,
     CURLOPT_HTTPHEADER => array('Content-type: application/xml', 'Accept: application/xml'),
-    CURLOPT_RETURNTRANSFER => 1
-    
+    CURLOPT_RETURNTRANSFER => 1  
 ));
 
 //projects: Send the request & save response to $resp
@@ -41,9 +64,9 @@ foreach ($projects as $p) {
     	$name=(string)$p->name[0];
     	$pid=(string)$p->id[0];
     	$project_url=$basecamphq_url.'/projects/'.$p->id.'-'.trim(strtolower(str_replace(' ','-',$p->name)));
-    	
+
     	//save to $output
-    	$output[$pid]['url']=$project_url.'/todo_lists?utf8=✓&responsible_party='.$account_id;
+    	$output[$pid]['url']=$project_url.'/todo_lists?responsible_party='.$account_id;
     	$output[$pid]['projectname']=$name;
     	$output[$pid]['project_url']=$project_url;
     }
@@ -59,7 +82,7 @@ curl_setopt_array($curl, array(
 ));
 
 //todos: Send the request & save response to $resp
-$resp = curl_exec($curl);
+$resp=curl_exec($curl);
 
 //Close curl processes to clear up some resources
 curl_close($curl);
@@ -93,20 +116,51 @@ foreach($todos as $t) {
 	}
 }
 
-//let's startoutput as html
+//////////////////////////////////
+//format output/content
+$new_note_content='';
+//make output and save it to $new_note_content
 foreach($output as $o) {
 	$project_url=$o['project_url'];
-	echo '<a href="'.$o['url'].'">'.$o['projectname'].'</a><br/>
+	$url=$o['url'];
+	$projectname=$o['projectname'];
+	$new_note_content.='<a href="'.$url.'">'.$projectname.'</a><br/>
 	<ul>';
 		$lists=(!empty($o['lists'])?$o['lists']:array());
 		foreach($lists as $l) {
-			echo '<li>'.$l['listname'].'
-				<ul>';
+			$new_note_content.='<li>'.$l['listname'].'<ul>';
 					foreach($l['item']as $it) {
-						echo '<li><a href="'.$project_url.'/todo_items/'.$it['id'].'/comments">'.$it['content'].'</a></li>';
+						$id=$it['id'];
+						$content=(string)htmlspecialchars($it['content']);
+						$new_note_content.='<li><a href="'.$project_url.'/todo_items/'.$id.'/comments">'.$content.'</a></li>';
 					}
-			echo '</ul></li>';
+			$new_note_content.='</ul></li>';
 		}
-	echo '</ul>';
+	$new_note_content.='</ul>';
 }
+
+//////////////////////////////////
+//Do Evernote stuff now
+//require evernote SDK
+require_once $evernote_autoload.'autoload.php';
+
+//get evernote client
+$client = new \Evernote\Client($developerToken, $sandbox);
+
+//get the note object
+$note=$client->getNote($note_guid);
+
+//make an identical note object, with a different name
+$updated_note=$note;
+
+//set updated content
+$updated_note->content=$new_note_content;
+
+//save changes
+$client->replaceNote($note, $updated_note);
+//Done!!
+
+//////////////////////////////////
+//show the content that was saved
+echo $new_note_content;
 ?>
